2e5229ed42a0e4ffbdf7bbc976f4bcdacf14a91c
LE THE VIET <winviet10@gmail.com>
2021-10-14 01:21:35 +0700

    firstpush

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
+
+module.exports = ({
   reactStrictMode: true,
-}
+  images: {
+    domains: ['i.imgur.com'],
+  },
+  crossOrigin: 'anonymous',
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+  },
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '../static/assets/css/style.css'
+import '../static/assets/css/timeline.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
